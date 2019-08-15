318e6a9eaeb5569173e66a5fed3ca3ae1b7a4a41
Martikainen <oliver.martikainen@cgi.com>
2019-08-15 16:31:23 +0300

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  var [count, setCount] = useState(0)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Queue">
+        <p>aa</p>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <p>hello {count}</p>
+        </header>
+      </div>
     </div>
   );
 }
