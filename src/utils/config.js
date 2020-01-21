7376f5bd9379091ed418332a721dcaa10be0def4
Martikainen <oliver.martikainen@cgi.com>
2020-01-21 12:23:33 +0200

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,8 @@
-const baseUrl = process.env.REACT_APP_BACKEND_URL || './api'
-const baseOrigin = process.env.REACT_APP_BACKEND_ORIGIN | ''
+let baseUrl = './api'
+const baseOrigin = process.env.REACT_APP_BACKEND_ORIGIN || ''
+
+if(process.env.NODE_ENV === 'development') {
+    baseUrl = process.env.REACT_APP_BACKEND_URL || baseUrl
+}
 
 export default {baseUrl, baseOrigin}
