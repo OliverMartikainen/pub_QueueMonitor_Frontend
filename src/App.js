62c5856c220d1af6789b40f53d7899710db111a9
Martikainen <48790588+OliverMartikainen@users.noreply.github.com>
2021-05-20 18:25:19 +0300

    Update dashboardUpdater related states
    combine all states into 1 & add jsdoc typedefs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,41 @@ import { queueFormatter, agentFormatter } from './utils/formatters'
 import { changeProfileFunc, changeTeamFunc } from './events/buttonEvents'
 import './App.css'
 
-
+/**
+ * @typedef { Array<{ 
+ *  AgentId: Number, 
+ *  AgentName: String, 
+ *  Duration: Number, //time agent has had current status
+ *  Reason: String, //agents current status --> used in agent item color decision
+ *  Team: String //agents teamName
+ * }>} queue
+ * 
+ * @typedef { Array<{ 
+ *  ChannelId: Number, //not used
+ *  ChannelName: String, //not used
+ *  ContactType: 'PBX' | 'email',
+ *  Direction: String, //not used
+ *  ServiceName: String, 
+ *  ServiceId: Number, 
+ *  QueueLength: Number, 
+ *  MaxQueueTime: Number 
+ * }>} agents
+ * 
+ * @typedef {{
+ *     reportPBX: Array.<{
+ *          ContactsPieces: Number, //how many contacts received in total
+ *          ProcessedPieces: Number, // how many of them were successfully answered
+ *          ServiceId: Number, 
+ *          ServiceName: String
+ *      }>, 
+ *      reportEmail: Array.<{
+ *          ContactsPieces: Number, 
+ *          ProcessedPieces: Number,
+ *          ServiceId: Number, 
+ *          ServiceName: String
+ *      }>,
+ *  }} report
+ */
 
 /**
  * Checks for change in dataUpdateStatus (dataUpdate feeds status code)
@@ -40,27 +74,44 @@ const defaultAlarms = () => {
 
 const App = () => { //Change activeTeam to shownAgents --> [AgentIds] --> agentfilter OR add this and keep activeTeam for options filter only?
     const [activeTeam, setActiveTeam] = useState(defaultTeam) //[TeamNames] - String --> AgentFilter - database doesnt provide TeamIds
-    const [activeProfileId, setQueueProfile] = useState(defaultProfile) //[ServiceIds] - Int --> QueueFilter
     const [activeAlarms, setActiveAlarms] = useState(defaultAlarms) /*{ServiceId: AlarmType} ServiceIds are unique numbers, Alarm type is 0-2 */
-    const [censor, setCensor] = useState(false) //boolean: if sensitive info needs to be hidden
-    const [queue, setQueue] = useState([]) //[{ServiceName, SerivceId, ContactType, QueueLength, MaxQueueTime}]
-    const [agents, setAgents] = useState([]) //for agent updates - show ones filtered by team
+    const [activeProfileId, setQueueProfile] = useState(defaultProfile) //[ServiceIds] - Int --> QueueFilter
+
+    //backends dataStream data storer
+    /**
+     * @type {[
+     *  { queue: queue, agents: agents, report: report },
+     *  React.Dispatch<React.SetStateAction<{ queue: queue, agents: agents, report: report }>>
+     * ]}
+     */
+    const [dashboardData, setDasboardData] = useState({
+        queue: [], //[{ServiceName, SerivceId, ContactType, QueueLength, MaxQueueTime}]
+        agents: [], //for agent updates - show ones filtered by team
+        report: { reportPBX: [], reportEmail: [] } /*used for statistics --> { reportPBX: [{ContactsPieces: Number, ProcessedPieces: Number, ServiceId: Number, ServiceName: String}], reportEmail: [{same}] } */
+    })
+
+
+    //backends teamUpdater data storer
     const [teams, setTeams] = useState([]) //[{TeamName, Profiles[same as queueProfile]}]: list of teams and their chosen services
-    const [report, setReport] = useState('')
     const [services, setServices] = useState([]) /* [{ServiceName, ServiceId}]  - used in OptionsSection ServiceAlarmsModal*/
+
     //200 OK, 502 database-backend error, 503 backend-frontend error --> combine for custom hook?
     const [connectionStatus, setConnectionStatus] = useState({ status: 200, errorStart: '' }) //{ Status: (200 or 502 or 503), ErrorStart: Date.ISOString} - using only DataUpdates to set error
     const [dataUpdateStatus, setDataUpdateStatus] = useState(200)
 
+    const [censor, setCensor] = useState(false) //boolean: if sensitive info needs to be hidden
+
+    //initiates SSE data update feed listeners
+    useEffect(() => {
+        teamUpdater(setTeams, setServices)
+        dashboardUpdater(setDasboardData, setDataUpdateStatus)
+    }, [])
 
     useEffect(() => {
         errorChecker(dataUpdateStatus, connectionStatus, setConnectionStatus)
     }, [dataUpdateStatus, connectionStatus])
 
-    useEffect(() => {
-        teamUpdater(setTeams, setServices)
-        dashboardUpdater(setQueue, setAgents, setReport, setDataUpdateStatus)
-    }, [])
+    const { queue, agents, report } = dashboardData
 
     const agentsFormatted = agentFormatter(activeTeam, agents, censor, teams)
     const queueFormatted = queueFormatter(queue, activeProfileId, teams, censor)
--- src/streams/dataUpdaters.js
+++ src/streams/dataUpdaters.js
@@ -1,6 +1,6 @@
 import eventService from '../services/eventService'
 
-export const dashboardUpdater = (setQueue, setAgents, setReport, setDataUpdateStatus) => {
+export const dashboardUpdater = (setDasboardData, setDataUpdateStatus) => {
     let dataUpdates = eventService.getDataUpdates()
     dataUpdates.onopen = () => {
         const time = new Date().toISOString().substr(11, 8)
@@ -12,27 +12,34 @@ export const dashboardUpdater = (setQueue, setAgents, setReport, setDataUpdateSt
         setDataUpdateStatus(503)
         dataUpdates.close() //without this & the setTimeout() firefox will close connection on 2nd error
         setTimeout(
-            () => dashboardUpdater(setQueue, setAgents, setReport, setDataUpdateStatus)
+            () => dashboardUpdater(setDasboardData, setDataUpdateStatus)
             , 10000)
     }
     dataUpdates.onmessage = (event) => {
-        const data = JSON.parse(event.data)
+        try {
+            const data = JSON.parse(event.data)
 
-        if (data.status !== 200) {
-            const time = new Date().toISOString()
-            setDataUpdateStatus(data.status)
-            console.log('TEAM UPDATE FAILED', data.status, time)
-            return
+            if (data.status !== 200) {
+                const time = new Date().toISOString()
+                setDataUpdateStatus(data.status)
+                console.log('TEAM UPDATE FAILED', data.status, time)
+                return
+            }
+
+            setDasboardData({
+                agents: data.agentsOnline,
+                queue: data.queue,
+                report: {
+                    reportPBX: data.reportPBX,
+                    reportEmail: data.reportEmail
+                }
+            })
+
+            setDataUpdateStatus(200)
+
+        } catch (error) {
+            console.error(error)
         }
-        //console.log(`dataUpdates MESSAGE: `, data.timeStamp)
-        const report = {
-            reportPBX: data.reportPBX,
-            reportEmail: data.reportEmail
-        }
-        setQueue(data.queue)
-        setAgents(data.agentsOnline)
-        setReport(report)
-        setDataUpdateStatus(200)
     }
 }
 
@@ -53,22 +60,26 @@ export const teamUpdater = (setTeams, setServices) => {
             , 10000)
     }
     teamUpdates.onmessage = (event) => {
-        const data = JSON.parse(event.data)
+        try {
+            const data = JSON.parse(event.data)
 
-        if (data.status !== 200) {
-            const time = new Date().toISOString().substr(11, 8)
-            console.log('TEAM UPDATE FAILED', data.status, time)
-            return
+            if (data.status !== 200) {
+                const time = new Date().toISOString().substr(11, 8)
+                console.log('TEAM UPDATE FAILED', data.status, time)
+                return
+            }
+
+            const serverVersion = window.sessionStorage.getItem('serverVersion') //restarts on browser open if it has old version stored - could avoid with close browser actions
+            if (serverVersion && (serverVersion !== data.serverVersion)) {  //if there is a stored server version compare it to data.serverVersion and refresh client if different
+                console.log('New version available', data.serverVersion, 'old version:', serverVersion)
+                setTimeout(() => { window.location.reload() }, 5000)
+            }
+
+            window.sessionStorage.setItem('serverVersion', data.serverVersion)
+            setTeams(data.teams)
+            setServices(data.services)
+        } catch (error) {
+            console.error(error)
         }
-
-        const serverVersion = window.sessionStorage.getItem('serverVersion') //restarts on browser open if it has old version stored - could avoid with close browser actions
-        if (serverVersion && (serverVersion !== data.serverVersion)) {  //if there is a stored server version compare it to data.serverVersion and refresh client if different
-            console.log('New version available', data.serverVersion, 'old version:', serverVersion)
-            setTimeout(() => { window.location.reload() }, 5000)
-        }
-
-        window.sessionStorage.setItem('serverVersion', data.serverVersion)
-        setTeams(data.teams)
-        setServices(data.services)
     }
 }
